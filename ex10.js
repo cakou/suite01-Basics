0e5456dc3661e2fe5f1513843270167eef509e07
cakou <carole.hermouet@gmail.com>
2017-03-29 15:03:47 +0200

    Update ex10.js

--- ex10.js
+++ ex10.js
@@ -17,21 +17,3 @@
         - une fonction "isAdult" pour tester si la personne est majeur: retourne true si l'age de la personne est d'au moins 18 ans, false si en dessous;
 
 */
-let person = {
-    name: {
-        firstName: "toto",
-        lastName: "tata"
-    }
-age: 45,
-
-fullName: function ()
- {
-     return this.name.firstName + "" + this.name.lastName;
- }
-};
-
-function getHumanTypeByAge (nb) {
-    if (nb >= 18) {
-        return "majeur"
-    }
-}
